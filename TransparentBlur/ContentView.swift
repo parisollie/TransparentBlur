31dcf0c5d56f874ef45ad2b0e2c8d102bbbecb7c
parisollie <pjff18@gmail.com>
2025-03-27 17:43:20 -0600

    Initial Commit

---
+++ TransparentBlur/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TransparentBlur
+//
+//  Created by Paul F on 27/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TransparentBlur/TransparentBlurApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentBlurApp.swift
+//  TransparentBlur
+//
+//  Created by Paul F on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TransparentBlurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
